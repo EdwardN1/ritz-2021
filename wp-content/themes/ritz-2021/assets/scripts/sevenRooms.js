33a4f1d49b00fa3f91571b9350f6bd6cfa377aea
EdwardN1 <edward@technicks.com>
2024-12-04 10:50:50 +0000

    Download of current active theme files

--- wp-content/themes/ritz-2021/assets/scripts/sevenRooms.js
+++ wp-content/themes/ritz-2021/assets/scripts/sevenRooms.js
@@ -2,13 +2,25 @@ jQuery(document).ready(function ($) {
     $('.ritz-seven-rooms').each(function () {
         let thisVenueID = $(this).data("venueid");
         let thistriggerId = $(this).attr("id");
-        SevenroomsWidget.init({
-            venueId: thisVenueID,
-            triggerId: thistriggerId, // id of the dom element that will trigger this widget
-            type: "reservations", // either 'reservations' or 'waitlist' or 'events'
-            styleButton: false, // true if you are using the SevenRooms button
-            clientToken: "" //(Optional) Pass the api generated clientTokenId here
-        })
+        if(thisVenueID=='ritzafternoontea') {
+            SevenroomsWidget.init({
+                venueId: thisVenueID,
+                allVenues: ['ritzafternoontea', 'theritzrestaurant'],
+                triggerId: thistriggerId, // id of the dom element that will trigger this widget
+                type: "reservations", // either 'reservations' or 'waitlist' or 'events'
+                styleButton: false, // true if you are using the SevenRooms button
+                clientToken: "" //(Optional) Pass the api generated clientTokenId here
+            })
+        } else {
+            SevenroomsWidget.init({
+                venueId: thisVenueID,
+                triggerId: thistriggerId, // id of the dom element that will trigger this widget
+                type: "reservations", // either 'reservations' or 'waitlist' or 'events'
+                styleButton: false, // true if you are using the SevenRooms button
+                clientToken: "" //(Optional) Pass the api generated clientTokenId here
+            })
+        }
+
     });
 
 });
